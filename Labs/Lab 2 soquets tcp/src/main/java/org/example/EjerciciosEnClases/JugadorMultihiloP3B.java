0a0ad4912e9f3375923de78d7515f35bf3be6f4f
ricardo <diazbarronricardojairo@gmial.com>
2024-03-11 23:18:12 -0400

    lab 2 soquets tcp

---
+++ Labs/Lab 2 soquets tcp/src/main/java/org/example/EjerciciosEnClases/JugadorMultihiloP3B.java
@@ -0,0 +1,46 @@
+package org.example.EjerciciosEnClases;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class JugadorMultihiloP3B {
+    public static void main(String[] args) throws IOException {
+        try {
+            Scanner sc = new Scanner(System.in);
+            InetAddress ip = InetAddress.getByName("localhost");
+            // establish the connection with server port 5056
+            Socket s = new Socket(ip, 5056);
+            // obtaining input and out streams
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            System.out.println("Bienvenido al juego de preguntas y respuestas, espera la pregunta del servidor");
+                    while (true) {
+                        try {
+                            String mensaje = dis.readUTF();
+                            System.out.println("Servidor pregunta:  " + mensaje);
+                            System.out.println("Respuesta: ");
+                            String mensaje2 = sc.nextLine();
+                            dos.writeUTF(mensaje2);
+                            if(mensaje.equals("salir")){
+                                System.out.println("Cerrando esta conexión : " + s);
+                                s.close();
+                                System.out.println("Conexión cerrada");
+                                break;
+                            }
+                            String mensaje3 = dis.readUTF();
+                            System.out.println("Servidor dice  resutlado de tu respuesta es:  " + mensaje3);
+
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                }
+            catch (Exception e) {
+
+        }}}
---
+++ Labs/Lab 2 soquets tcp/src/main/java/org/example/EjerciciosEnClases/ServerMultihiloP1.java
@@ -0,0 +1,45 @@
+package org.example.EjerciciosEnClases;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerMultihiloP1
+{
+
+    public static ArrayList<ManejadorClienteP1> clientesConectados = new ArrayList<ManejadorClienteP1>();
+    public static void main(String[] args) throws IOException
+    {
+
+        ServerSocket ss = new ServerSocket(5056);
+        while (true)
+        {
+            Socket s = null;
+            try
+            {
+
+                s = ss.accept();
+                System.out.println("un nuevo cliente se ha conectado : " + s);
+
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+                System.out.println("Asignar unnuevo hilopara este cliente ");
+                ManejadorClienteP1 mc = new ManejadorClienteP1(s, dis, dos);
+                Thread t = new Thread(mc);
+
+                System.out.println("Asignando un nuevo hilo para este cliente");
+                clientesConectados.add(mc);
+                t.start();
+            }
+            catch (Exception e){
+                s.close();
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
